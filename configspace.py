024b7ee9e2bd2dcf5803a444e754a775badb7d27
FlorianGrabowski <flo.grabowski.98@gmail.com>
2022-05-09 11:04:24 +0200

    integrated collision image in configspace, removed remainders of old subtractions, implemented restart

--- configspace.py
+++ configspace.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas, CENTER
+from tkinter import CENTER
 from PIL import Image
 
 
@@ -12,14 +12,14 @@ class Configspace:  # shows the way of the robot the algorithm
         self.root = root  # setting the root of the second page for access on the Canvas.
         self.xExt = 0  # Max x
         self.yExt = 0  # Max y
-        self.canvas = Canvas(self.root)  # Canvas for 2D graphics.
-        self.theOffsetX = Image.open(robotImagePath).width  # haf of the pixel of the robot png.
-        self.theOffsetY = Image.open(robotImagePath).height
+        self.canvas = root.winfo_children()[0]  # Canvas for 2D graphics.
+        self.theOffsetX = int(Image.open(robotImagePath).width/2)  # haf of the pixel of the robot png.
+        self.theOffsetY = int(Image.open(robotImagePath).height/2)
 
     def setDimensions(self, x, y):  # of the canvas
         self.xExt = x  # set Max x
         self.yExt = y  # set Max Y
-        self.canvas.config(bd=0, height=y + 2 * self.theOffsetY, width=x + 2 * self.theOffsetX)  # bd = Border just
+        self.canvas.config(bd=0, height=y, width=x)  # bd = Border just
         # setting up the canvas dimensions + offset 2 times so the center of the set Robot is always inside.
         self.drawSpace()  # actually drawing the canvas with borders and solution path
         self.canvas.place(relx=0.5, rely=0.5, anchor=CENTER)  # placing the canvas on the second page
@@ -27,13 +27,15 @@ class Configspace:  # shows the way of the robot the algorithm
 
     def drawSpace(self):
         # Open: remove offset, canvas.config offset buggy (NOT MY COMMENT)
-        self.canvas.delete("all")  # deletes all drawings from the canvas
+        # self.canvas.delete("all")  # deletes all drawings from the canvas
         y = self.yExt  # set local y to Max y of the
         x = self.xExt  # set local x to Max x of the
-        self.canvas.create_line(self.theOffsetX, self.theOffsetY, self.theOffsetX, y + self.theOffsetY)  # left border
-        self.canvas.create_line(self.theOffsetX, self.theOffsetY, x + self.theOffsetX, self.theOffsetY)  # upper border
-        self.canvas.create_line(x + self.theOffsetX, y + self.theOffsetY, x + self.theOffsetX, self.theOffsetY)  # right
-        self.canvas.create_line(x + self.theOffsetX, y + self.theOffsetY, self.theOffsetX, y + self.theOffsetY)  # lower
+        self.canvas.create_line(self.theOffsetX, self.theOffsetY, self.theOffsetX, y - self.theOffsetY, fill='red')  # left border
+        self.canvas.create_line(self.theOffsetX, self.theOffsetY, x - self.theOffsetX, self.theOffsetY, fill='red')  # upper border
+        self.canvas.create_line(x - self.theOffsetX, y - self.theOffsetY, x - self.theOffsetX, self.theOffsetY,
+                                fill='red')  # right
+        self.canvas.create_line(x - self.theOffsetX, y - self.theOffsetY, self.theOffsetX, y - self.theOffsetY,
+                                fill='red')  # lower
 
         if len(self.solutionPath) > 0: self.drawSolutionPath()  # if the Solution Path is calculated draw it.
         if self.initConfig[0] > -1: self.drawConfiguration(self.initConfig[0], self.initConfig[1], 'green')  # if the
@@ -43,18 +45,18 @@ class Configspace:  # shows the way of the robot the algorithm
 
     def drawConfiguration(self, x, y, color):  # draws a dot on the Canvas (used above for the start and goal)
         r = 5  # radius of the colored dot
-        self.canvas.create_oval(x - r + self.theOffsetX,
-                                y - r + self.theOffsetY,
-                                x + r + self.theOffsetX,
-                                y + r + self.theOffsetY,
+        self.canvas.create_oval(x - r,
+                                y - r,
+                                x + r,
+                                y + r,
                                 fill=color)  # draw color
 
     def drawSolutionPath(self):  # Draws a line connecting all the points from the solution-path
         for i in range(1, len(self.solutionPath)):  # iterate over points from solution-path
             c1 = self.solutionPath[i - 1]  # c1 is the point for the start of the line at loop cycle i
             c2 = self.solutionPath[i]  # c2 is the point for the end of the line at loop cycle i
-            self.canvas.create_line(c1[0] + self.theOffsetX, c1[1] + self.theOffsetY,
-                                    c2[0] + self.theOffsetX, c2[1] + self.theOffsetY, fill='purple1')
+            self.canvas.create_line(c1[0], c1[1],
+                                    c2[0], c2[1], fill='purple1')
             # draws line from c1 to c2 in purple color
 
     def setIntialSolutionPath(self):  # fills the self.solutionpath array with points in a straight line from start
